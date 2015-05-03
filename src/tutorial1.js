36f1afd0c655169fd8fbed3304997c191e7f44fd
shinichi yamaguchi <shinichi62@gmail.com>
2015-05-03 11:58:01 +0900

    Using props

--- src/tutorial1.js
+++ src/tutorial1.js
@@ -1,13 +1,25 @@
-var CommentList = React.createClass({
+var Comment = React.createClass({
   render: function() {
     return (
-      <div className="commentList">
-        Hello, world! I am a CommentList.
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
       </div>
     );
   }
 });
-
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
 var CommentForm = React.createClass({
   render: function() {
     return (
